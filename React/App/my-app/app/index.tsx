6443c6c01520641ea90af1f9417d81f6d541a547
Richard-Raph <richardakpan77@gmail.com>
2025-01-02 14:26:47 +0100

    `Refactored HomeScreen component: removed unused imports, simplified state and rendering, and updated styles`

--- React/App/my-app/app/index.tsx
+++ React/App/my-app/app/index.tsx
@@ -1,76 +1,42 @@
 import { useState } from 'react';
-import { View, Text, TextInput, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import Header from './components/Header';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 export default function HomeScreen() {
-    const [age, setAge] = useState('30');
-    const [name, setName] = useState('World');
-    const [people, setPeople] = useState([
-        { name: 'Shaun', id: '1' },
-        { name: 'Roshi', id: '2' },
-        { name: 'Mario', id: '3' },
-        { name: 'Luigi', id: '4' },
-        { name: 'Peach', id: '5' },
-        { name: 'Toady', id: '6' },
-        { name: 'Bower', id: '7' },
+    const [todos, setTodos] = useState([
+        { text: 'buy coffee', key: '1' },
+        { text: 'create an app', key: '2' },
+        { text: 'play on the switch', key: '3' },
     ]);
-
-    const pressHandler = (id: string) => {
-        console.log(id);
-        setPeople(prevPeople => {
-            return prevPeople.filter(person => person.id != id);
-        })
-    }
 
     return (
         <View style={styles.container}>
-            <Text>Enter name: </Text>
-            <TextInput
-                multiline
-                style={styles.input}
-                placeholder='John Doe'
-                onChangeText={(val: string) => setName(val)}
-            />
-            <Text>Enter Age: </Text>
-            <TextInput
-                placeholder='99'
-                style={styles.input}
-                keyboardType='numeric'
-                onChangeText={(val) => setAge(val)}
-            />
-            <Text>Name: {name}, Age: {age}</Text>
-
-            <FlatList
-                data={people}
-                numColumns={3}
-                keyExtractor={item => item.id}
-                renderItem={({ item }) => (
-                    <TouchableOpacity onPress={() => pressHandler(item.id)}>
-                        <Text style={styles.list}>{item.name}</Text>
-                    </TouchableOpacity>
-                )}
-            />
+            {/* header */}
+            <Header />
+            <View style={styles.content}>
+                {/* Todo form */}
+                <View style={styles.list}>
+                    <FlatList
+                        data={todos}
+                        renderItem={({ item }) => (
+                            <Text>{item.text}</Text>
+                        )}
+                    />
+                </View>
+            </View>
         </View>
-    );
+    )
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        paddingTop: 30,
-        paddingHorizontal: 20,
+        backgroundColor: '#fff',
     },
-    input: {
-        width: 200,
-        margin: 10,
-        padding: 8,
-        borderWidth: 1,
-        borderColor: '#777',
+    content: {
+        padding: 40,
     },
     list: {
-        padding: 10,
-        fontSize: 20,
         marginTop: 20,
-        marginHorizontal: 5,
-        backgroundColor: 'pink',
     },
-});
+})
