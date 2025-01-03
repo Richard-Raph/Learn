9e3c25b94c9dbae1373b6f805c2e6e080430d9c2
Richard-Raph <richardakpan77@gmail.com>
2025-01-03 05:28:26 +0100

    `Removed unused code and components from todo-app and created new project`

---
+++ React/App/gamezone/screens/Review.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function Review() {
+    return (
+        <View style={styles.container}>
+            <Text>Review Screen</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 24,
+    },
+});
