21c19d2459cb1e60b26e3747160b48ccfd6e0968
Richard-Raph <richardakpan77@gmail.com>
2025-01-03 13:51:09 +0100

    `Update Home and Review screens updated with navigation prop changes`

--- React/App/gamezone/screens/Home.tsx
+++ React/App/gamezone/screens/Home.tsx
@@ -3,17 +3,20 @@ import { globalStyles } from '../styles/Global';
 import { View, Text, Button } from 'react-native';
 
 interface NavigationProps {
-    navigation: (navigate: Function) => void;
+    navigation: {
+        navigate: (screen: string) => void;
+    };
 }
 
 export default function Home({ navigation }: NavigationProps) {
     const [reviews, setReviews] = useState([
-        { title: '', rating: 5, body: '', key: '1' }
+        { title: 'Zelda, Breath of Fresh Air', rating: 5, body: 'lorem ipsum', key: '1' },
+        { title: 'Gotta Catch Them All (again)', rating: 4, body: 'lorem ipsum', key: '2' },
+        { title: 'Not so "Final" Fantasy', rating: 3, body: 'lorem ipsum', key: '3' },
     ]);
     
     const pressHandler = () => {
         navigation.navigate('Review');
-        // navigation.push('Review');
     }
 
     return (
--- React/App/gamezone/screens/Review.tsx
+++ React/App/gamezone/screens/Review.tsx
@@ -2,7 +2,9 @@ import { globalStyles } from '../styles/Global';
 import { View, Text, Button } from 'react-native';
 
 interface NavigationProps {
-    navigation: (goBack: Function) => void;
+    navigation: {
+        goBack: () => void;
+    }
 }
 
 export default function Review({ navigation }: NavigationProps) {
