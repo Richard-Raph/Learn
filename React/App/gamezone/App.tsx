d701291e9563cac08dcfa2ce7604b71261412b76
Richard-Raph <richardakpan77@gmail.com>
2025-01-03 06:33:19 +0100

    `feat: updated App.tsx to use expo-splash-screen and expo-font, added Home screen with Nunito font`

--- React/App/gamezone/App.tsx
+++ React/App/gamezone/App.tsx
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Text } from 'react-native';
+import Home from './screens/Home';
+import { useEffect } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+import { Nunito_700Bold, Nunito_400Regular, useFonts } from '@expo-google-fonts/nunito';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [loaded, error] = useFonts({
+    Nunito_700Bold,
+    Nunito_400Regular,
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Home />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#fff',
-  },
-});
--- React/App/gamezone/screens/Home.tsx
+++ React/App/gamezone/screens/Home.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, View, Text } from 'react-native';
 export default function Home() {
     return (
         <View style={styles.container}>
-            <Text>Home Screen</Text>
+            <Text style={{ fontFamily: 'Nunito_700Bold' }}>Home Screen</Text>
         </View>
     );
 }
