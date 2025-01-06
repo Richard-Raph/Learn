ca392e0d039a7ea4536826cd3549e408c8664fb3
Richard-Raph <richardakpan77@gmail.com>
2025-01-06 16:19:40 +0100

    deleted node mules and package.json from old project and changed the nme to mistake

--- React/App/gamezone/App.tsx
+++ React/App/gamezone/App.tsx
@@ -1,25 +1,20 @@
-import { useEffect } from 'react';
-import Navigator from './routes/Drawer';
-import * as SplashScreen from 'expo-splash-screen';
-import { Nunito_700Bold, Nunito_400Regular, useFonts } from '@expo-google-fonts/nunito';
-
-SplashScreen.preventAutoHideAsync();
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const [loaded, error] = useFonts({
-    Nunito_700Bold,
-    Nunito_400Regular,
-  });
-
-  useEffect(() => {
-    if (loaded || error) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded, error]);
-
-  if (!loaded && !error) {
-    return null;
-  }
-
-  return <Navigator />
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ React/App/mistake/App.tsx
@@ -0,0 +1,25 @@
+import { useEffect } from 'react';
+import Navigator from './routes/Drawer';
+import * as SplashScreen from 'expo-splash-screen';
+import { Nunito_700Bold, Nunito_400Regular, useFonts } from '@expo-google-fonts/nunito';
+
+SplashScreen.preventAutoHideAsync();
+
+export default function App() {
+  const [loaded, error] = useFonts({
+    Nunito_700Bold,
+    Nunito_400Regular,
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
+  return <Navigator />
+}
